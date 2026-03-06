7d1f3e7574003cd201941d10ca26c31924205e09
Intak Hwang <sp301415@gmail.com>
2026-03-06 14:52:30 +0900

    chore: Cleanup NTT code

--- math/dft/asm_ntt_pow2.go
+++ math/dft/asm_ntt_pow2.go
@@ -9,19 +9,22 @@ import (
 // fwdNTTInPlacePow2Unroll computes the NTT transform in-place for power-of-two length coefficients.
 // Assumes len(coeffs) >= 32.
 func fwdNTTInPlacePow2Unroll(coeffs, tw, twS []uint64, q uint64) {
-	N := len(coeffs)
 	twoQ := q << 1
-	twPtr := unsafe.Pointer(unsafe.SliceData(tw))
-	twSPtr := unsafe.Pointer(unsafe.SliceData(twS))
-	coeffsPtr := unsafe.Pointer(unsafe.SliceData(coeffs))
+
+	N := len(coeffs)
+	L := unsafe.Sizeof(uint64(0))
+
+	r := unsafe.Pointer(unsafe.SliceData(tw))
+	rS := unsafe.Pointer(unsafe.SliceData(twS))
+	v := unsafe.Pointer(unsafe.SliceData(coeffs))
 	var w, wS uint64
 
 	t := N / 2
-	w = *(*uint64)(unsafe.Add(twPtr, 1*unsafe.Sizeof(uint64(0))))
-	wS = *(*uint64)(unsafe.Add(twSPtr, 1*unsafe.Sizeof(uint64(0))))
+	w = *(*uint64)(unsafe.Add(r, 1*L))
+	wS = *(*uint64)(unsafe.Add(rS, 1*L))
 	for j := 0; j < N/2; j += 8 {
-		c0 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
-		c1 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j+t)*unsafe.Sizeof(uint64(0))))
+		c0 := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
+		c1 := (*[8]uint64)(unsafe.Add(v, uintptr(j+t)*L))
 
 		c0[0], c1[0] = butterflyPow2(c0[0], c1[0], w, wS, q, twoQ)
 		c0[1], c1[1] = butterflyPow2(c0[1], c1[1], w, wS, q, twoQ)
@@ -40,12 +43,12 @@ func fwdNTTInPlacePow2Unroll(coeffs, tw, twS []uint64, q uint64) {
 			j1 := i * t << 1
 			j2 := j1 + t
 
-			w = *(*uint64)(unsafe.Add(twPtr, uintptr(m+i)*unsafe.Sizeof(uint64(0))))
-			wS = *(*uint64)(unsafe.Add(twSPtr, uintptr(m+i)*unsafe.Sizeof(uint64(0))))
+			w = *(*uint64)(unsafe.Add(r, uintptr(m+i)*L))
+			wS = *(*uint64)(unsafe.Add(rS, uintptr(m+i)*L))
 
 			for j := j1; j < j2; j += 8 {
-				c0 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
-				c1 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j+t)*unsafe.Sizeof(uint64(0))))
+				c0 := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
+				c1 := (*[8]uint64)(unsafe.Add(v, uintptr(j+t)*L))
 
 				c0[0], c1[0] = butterflyPow2(c0[0], c1[0], w, wS, q, twoQ)
 				c0[1], c1[1] = butterflyPow2(c0[1], c1[1], w, wS, q, twoQ)
@@ -62,12 +65,12 @@ func fwdNTTInPlacePow2Unroll(coeffs, tw, twS []uint64, q uint64) {
 
 	// t = 4, m = N / 8
 	for i := 0; i < N/8; i++ {
-		w = *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/8)*unsafe.Sizeof(uint64(0))))
-		wS = *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/8)*unsafe.Sizeof(uint64(0))))
+		w = *(*uint64)(unsafe.Add(r, uintptr(i+N/8)*L))
+		wS = *(*uint64)(unsafe.Add(rS, uintptr(i+N/8)*L))
 
 		j := 8 * i
 
-		c := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
 
 		c[0], c[4] = butterflyPow2(c[0], c[4], w, wS, q, twoQ)
 		c[1], c[5] = butterflyPow2(c[1], c[5], w, wS, q, twoQ)
@@ -79,15 +82,15 @@ func fwdNTTInPlacePow2Unroll(coeffs, tw, twS []uint64, q uint64) {
 	for i := 0; i < N/4; i += 2 {
 		j := 4 * i
 
-		c := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
 
-		w = *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/4)*unsafe.Sizeof(uint64(0))))
-		wS = *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/4)*unsafe.Sizeof(uint64(0))))
+		w = *(*uint64)(unsafe.Add(r, uintptr(i+N/4)*L))
+		wS = *(*uint64)(unsafe.Add(rS, uintptr(i+N/4)*L))
 		c[0], c[2] = butterflyPow2(c[0], c[2], w, wS, q, twoQ)
 		c[1], c[3] = butterflyPow2(c[1], c[3], w, wS, q, twoQ)
 
-		w = *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/4+1)*unsafe.Sizeof(uint64(0))))
-		wS = *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/4+1)*unsafe.Sizeof(uint64(0))))
+		w = *(*uint64)(unsafe.Add(r, uintptr(i+N/4+1)*L))
+		wS = *(*uint64)(unsafe.Add(rS, uintptr(i+N/4+1)*L))
 		c[4], c[6] = butterflyPow2(c[4], c[6], w, wS, q, twoQ)
 		c[5], c[7] = butterflyPow2(c[5], c[7], w, wS, q, twoQ)
 	}
@@ -96,62 +99,65 @@ func fwdNTTInPlacePow2Unroll(coeffs, tw, twS []uint64, q uint64) {
 	for i := 0; i < N/2; i += 4 {
 		j := 2 * i
 
-		c := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
 
-		c[0], c[1] = butterflyPow2(c[0], c[1], *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/2+0)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/2+0)*unsafe.Sizeof(uint64(0)))), q, twoQ)
-		c[2], c[3] = butterflyPow2(c[2], c[3], *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/2+1)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/2+1)*unsafe.Sizeof(uint64(0)))), q, twoQ)
-		c[4], c[5] = butterflyPow2(c[4], c[5], *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/2+2)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/2+2)*unsafe.Sizeof(uint64(0)))), q, twoQ)
-		c[6], c[7] = butterflyPow2(c[6], c[7], *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/2+3)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/2+3)*unsafe.Sizeof(uint64(0)))), q, twoQ)
+		c[0], c[1] = butterflyPow2(c[0], c[1], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+0)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+0)*L)), q, twoQ)
+		c[2], c[3] = butterflyPow2(c[2], c[3], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+1)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+1)*L)), q, twoQ)
+		c[4], c[5] = butterflyPow2(c[4], c[5], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+2)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+2)*L)), q, twoQ)
+		c[6], c[7] = butterflyPow2(c[6], c[7], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+3)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+3)*L)), q, twoQ)
 	}
 }
 
 // invNTTInPlacePow2Unroll computes the Inverse NTT transform in-place for power-of-two length coefficients.
 // Assumes len(coeffs) >= 32.
 func invNTTInPlacePow2Unroll(coeffs, twInv, twInvS []uint64, q uint64) {
-	N := len(coeffs)
 	twoQ := q << 1
-	twInvPtr := unsafe.Pointer(unsafe.SliceData(twInv))
-	twInvSPtr := unsafe.Pointer(unsafe.SliceData(twInvS))
-	coeffsPtr := unsafe.Pointer(unsafe.SliceData(coeffs))
+
+	N := len(coeffs)
+	L := unsafe.Sizeof(uint64(0))
+
+	r := unsafe.Pointer(unsafe.SliceData(twInv))
+	rS := unsafe.Pointer(unsafe.SliceData(twInvS))
+	v := unsafe.Pointer(unsafe.SliceData(coeffs))
 	var w, wS uint64
 
 	// t = 1, m = N / 2
 	for i := 0; i < N/2; i += 4 {
 		j := 2 * i
 
-		c := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
 
-		c[0], c[1] = invButterflyPow2(c[0], c[1], *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/2+0)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/2+0)*unsafe.Sizeof(uint64(0)))), q, twoQ)
-		c[2], c[3] = invButterflyPow2(c[2], c[3], *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/2+1)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/2+1)*unsafe.Sizeof(uint64(0)))), q, twoQ)
-		c[4], c[5] = invButterflyPow2(c[4], c[5], *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/2+2)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/2+2)*unsafe.Sizeof(uint64(0)))), q, twoQ)
-		c[6], c[7] = invButterflyPow2(c[6], c[7], *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/2+3)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/2+3)*unsafe.Sizeof(uint64(0)))), q, twoQ)
+		c[0], c[1] = invButterflyPow2(c[0], c[1], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+0)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+0)*L)), q, twoQ)
+		c[2], c[3] = invButterflyPow2(c[2], c[3], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+1)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+1)*L)), q, twoQ)
+		c[4], c[5] = invButterflyPow2(c[4], c[5], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+2)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+2)*L)), q, twoQ)
+		c[6], c[7] = invButterflyPow2(c[6], c[7], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+3)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+3)*L)), q, twoQ)
 	}
 
 	// t = 2, m = N / 4
 	for i := 0; i < N/4; i += 2 {
 		j := 4 * i
 
-		c := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
 
-		w = *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/4)*unsafe.Sizeof(uint64(0))))
-		wS = *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/4)*unsafe.Sizeof(uint64(0))))
+		w = *(*uint64)(unsafe.Add(r, uintptr(i+N/4)*L))
+		wS = *(*uint64)(unsafe.Add(rS, uintptr(i+N/4)*L))
 		c[0], c[2] = invButterflyPow2(c[0], c[2], w, wS, q, twoQ)
 		c[1], c[3] = invButterflyPow2(c[1], c[3], w, wS, q, twoQ)
 
-		w = *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/4+1)*unsafe.Sizeof(uint64(0))))
-		wS = *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/4+1)*unsafe.Sizeof(uint64(0))))
+		w = *(*uint64)(unsafe.Add(r, uintptr(i+N/4+1)*L))
+		wS = *(*uint64)(unsafe.Add(rS, uintptr(i+N/4+1)*L))
 		c[4], c[6] = invButterflyPow2(c[4], c[6], w, wS, q, twoQ)
 		c[5], c[7] = invButterflyPow2(c[5], c[7], w, wS, q, twoQ)
 	}
 
 	// t = 4, m = N / 8
 	for i := 0; i < N/8; i++ {
-		w = *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/8)*unsafe.Sizeof(uint64(0))))
-		wS = *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/8)*unsafe.Sizeof(uint64(0))))
+		w = *(*uint64)(unsafe.Add(r, uintptr(i+N/8)*L))
+		wS = *(*uint64)(unsafe.Add(rS, uintptr(i+N/8)*L))
 
 		j := 8 * i
 
-		c := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
 
 		c[0], c[4] = invButterflyPow2(c[0], c[4], w, wS, q, twoQ)
 		c[1], c[5] = invButterflyPow2(c[1], c[5], w, wS, q, twoQ)
@@ -165,12 +171,12 @@ func invNTTInPlacePow2Unroll(coeffs, twInv, twInvS []uint64, q uint64) {
 			j1 := i * t << 1
 			j2 := j1 + t
 
-			w = *(*uint64)(unsafe.Add(twInvPtr, uintptr(m+i)*unsafe.Sizeof(uint64(0))))
-			wS = *(*uint64)(unsafe.Add(twInvSPtr, uintptr(m+i)*unsafe.Sizeof(uint64(0))))
+			w = *(*uint64)(unsafe.Add(r, uintptr(m+i)*L))
+			wS = *(*uint64)(unsafe.Add(rS, uintptr(m+i)*L))
 
 			for j := j1; j < j2; j += 8 {
-				c0 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
-				c1 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j+t)*unsafe.Sizeof(uint64(0))))
+				c0 := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
+				c1 := (*[8]uint64)(unsafe.Add(v, uintptr(j+t)*L))
 
 				c0[0], c1[0] = invButterflyPow2(c0[0], c1[0], w, wS, q, twoQ)
 				c0[1], c1[1] = invButterflyPow2(c0[1], c1[1], w, wS, q, twoQ)
@@ -186,11 +192,11 @@ func invNTTInPlacePow2Unroll(coeffs, twInv, twInvS []uint64, q uint64) {
 		t <<= 1
 	}
 
-	w = *(*uint64)(unsafe.Add(twInvPtr, 1*unsafe.Sizeof(uint64(0))))
-	wS = *(*uint64)(unsafe.Add(twInvSPtr, 1*unsafe.Sizeof(uint64(0))))
+	w = *(*uint64)(unsafe.Add(r, 1*L))
+	wS = *(*uint64)(unsafe.Add(rS, 1*L))
 	for j := 0; j < N/2; j += 8 {
-		c0 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
-		c1 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j+t)*unsafe.Sizeof(uint64(0))))
+		c0 := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
+		c1 := (*[8]uint64)(unsafe.Add(v, uintptr(j+t)*L))
 
 		c0[0], c1[0] = invButterflyPow2(c0[0], c1[0], w, wS, q, twoQ)
 		c0[1], c1[1] = invButterflyPow2(c0[1], c1[1], w, wS, q, twoQ)
--- math/dft/asm_ntt_pow2_amd64.go
+++ math/dft/asm_ntt_pow2_amd64.go
@@ -19,19 +19,23 @@ func fwdNTTInPlacePow2Unroll(coeffs, tw, twS []uint64, q uint64) {
 		fwdNTTInPlacePow2UnrollAVX2(coeffs, tw, twS, q)
 		return
 	}
-	N := len(coeffs)
+
 	twoQ := q << 1
-	twPtr := unsafe.Pointer(unsafe.SliceData(tw))
-	twSPtr := unsafe.Pointer(unsafe.SliceData(twS))
-	coeffsPtr := unsafe.Pointer(unsafe.SliceData(coeffs))
+
+	N := len(coeffs)
+	L := unsafe.Sizeof(uint64(0))
+
+	r := unsafe.Pointer(unsafe.SliceData(tw))
+	rS := unsafe.Pointer(unsafe.SliceData(twS))
+	v := unsafe.Pointer(unsafe.SliceData(coeffs))
 	var w, wS uint64
 
 	t := N / 2
-	w = *(*uint64)(unsafe.Add(twPtr, 1*unsafe.Sizeof(uint64(0))))
-	wS = *(*uint64)(unsafe.Add(twSPtr, 1*unsafe.Sizeof(uint64(0))))
+	w = *(*uint64)(unsafe.Add(r, 1*L))
+	wS = *(*uint64)(unsafe.Add(rS, 1*L))
 	for j := 0; j < N/2; j += 8 {
-		c0 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
-		c1 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j+t)*unsafe.Sizeof(uint64(0))))
+		c0 := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
+		c1 := (*[8]uint64)(unsafe.Add(v, uintptr(j+t)*L))
 
 		c0[0], c1[0] = butterflyPow2(c0[0], c1[0], w, wS, q, twoQ)
 		c0[1], c1[1] = butterflyPow2(c0[1], c1[1], w, wS, q, twoQ)
@@ -50,12 +54,12 @@ func fwdNTTInPlacePow2Unroll(coeffs, tw, twS []uint64, q uint64) {
 			j1 := i * t << 1
 			j2 := j1 + t
 
-			w = *(*uint64)(unsafe.Add(twPtr, uintptr(m+i)*unsafe.Sizeof(uint64(0))))
-			wS = *(*uint64)(unsafe.Add(twSPtr, uintptr(m+i)*unsafe.Sizeof(uint64(0))))
+			w = *(*uint64)(unsafe.Add(r, uintptr(m+i)*L))
+			wS = *(*uint64)(unsafe.Add(rS, uintptr(m+i)*L))
 
 			for j := j1; j < j2; j += 8 {
-				c0 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
-				c1 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j+t)*unsafe.Sizeof(uint64(0))))
+				c0 := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
+				c1 := (*[8]uint64)(unsafe.Add(v, uintptr(j+t)*L))
 
 				c0[0], c1[0] = butterflyPow2(c0[0], c1[0], w, wS, q, twoQ)
 				c0[1], c1[1] = butterflyPow2(c0[1], c1[1], w, wS, q, twoQ)
@@ -72,12 +76,12 @@ func fwdNTTInPlacePow2Unroll(coeffs, tw, twS []uint64, q uint64) {
 
 	// t = 4, m = N / 8
 	for i := 0; i < N/8; i++ {
-		w = *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/8)*unsafe.Sizeof(uint64(0))))
-		wS = *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/8)*unsafe.Sizeof(uint64(0))))
+		w = *(*uint64)(unsafe.Add(r, uintptr(i+N/8)*L))
+		wS = *(*uint64)(unsafe.Add(rS, uintptr(i+N/8)*L))
 
 		j := 8 * i
 
-		c := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
 
 		c[0], c[4] = butterflyPow2(c[0], c[4], w, wS, q, twoQ)
 		c[1], c[5] = butterflyPow2(c[1], c[5], w, wS, q, twoQ)
@@ -89,15 +93,15 @@ func fwdNTTInPlacePow2Unroll(coeffs, tw, twS []uint64, q uint64) {
 	for i := 0; i < N/4; i += 2 {
 		j := 4 * i
 
-		c := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
 
-		w = *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/4)*unsafe.Sizeof(uint64(0))))
-		wS = *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/4)*unsafe.Sizeof(uint64(0))))
+		w = *(*uint64)(unsafe.Add(r, uintptr(i+N/4)*L))
+		wS = *(*uint64)(unsafe.Add(rS, uintptr(i+N/4)*L))
 		c[0], c[2] = butterflyPow2(c[0], c[2], w, wS, q, twoQ)
 		c[1], c[3] = butterflyPow2(c[1], c[3], w, wS, q, twoQ)
 
-		w = *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/4+1)*unsafe.Sizeof(uint64(0))))
-		wS = *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/4+1)*unsafe.Sizeof(uint64(0))))
+		w = *(*uint64)(unsafe.Add(r, uintptr(i+N/4+1)*L))
+		wS = *(*uint64)(unsafe.Add(rS, uintptr(i+N/4+1)*L))
 		c[4], c[6] = butterflyPow2(c[4], c[6], w, wS, q, twoQ)
 		c[5], c[7] = butterflyPow2(c[5], c[7], w, wS, q, twoQ)
 	}
@@ -106,12 +110,12 @@ func fwdNTTInPlacePow2Unroll(coeffs, tw, twS []uint64, q uint64) {
 	for i := 0; i < N/2; i += 4 {
 		j := 2 * i
 
-		c := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
 
-		c[0], c[1] = butterflyPow2(c[0], c[1], *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/2+0)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/2+0)*unsafe.Sizeof(uint64(0)))), q, twoQ)
-		c[2], c[3] = butterflyPow2(c[2], c[3], *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/2+1)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/2+1)*unsafe.Sizeof(uint64(0)))), q, twoQ)
-		c[4], c[5] = butterflyPow2(c[4], c[5], *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/2+2)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/2+2)*unsafe.Sizeof(uint64(0)))), q, twoQ)
-		c[6], c[7] = butterflyPow2(c[6], c[7], *(*uint64)(unsafe.Add(twPtr, uintptr(i+N/2+3)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twSPtr, uintptr(i+N/2+3)*unsafe.Sizeof(uint64(0)))), q, twoQ)
+		c[0], c[1] = butterflyPow2(c[0], c[1], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+0)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+0)*L)), q, twoQ)
+		c[2], c[3] = butterflyPow2(c[2], c[3], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+1)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+1)*L)), q, twoQ)
+		c[4], c[5] = butterflyPow2(c[4], c[5], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+2)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+2)*L)), q, twoQ)
+		c[6], c[7] = butterflyPow2(c[6], c[7], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+3)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+3)*L)), q, twoQ)
 	}
 }
 
@@ -127,50 +131,53 @@ func invNTTInPlacePow2Unroll(coeffs, twInv, twInvS []uint64, q uint64) {
 		return
 	}
 
-	N := len(coeffs)
 	twoQ := q << 1
-	twInvPtr := unsafe.Pointer(unsafe.SliceData(twInv))
-	twInvSPtr := unsafe.Pointer(unsafe.SliceData(twInvS))
-	coeffsPtr := unsafe.Pointer(unsafe.SliceData(coeffs))
+
+	N := len(coeffs)
+	L := unsafe.Sizeof(uint64(0))
+
+	r := unsafe.Pointer(unsafe.SliceData(twInv))
+	rS := unsafe.Pointer(unsafe.SliceData(twInvS))
+	v := unsafe.Pointer(unsafe.SliceData(coeffs))
 	var w, wS uint64
 
 	// t = 1, m = N / 2
 	for i := 0; i < N/2; i += 4 {
 		j := 2 * i
 
-		c := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
 
-		c[0], c[1] = invButterflyPow2(c[0], c[1], *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/2+0)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/2+0)*unsafe.Sizeof(uint64(0)))), q, twoQ)
-		c[2], c[3] = invButterflyPow2(c[2], c[3], *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/2+1)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/2+1)*unsafe.Sizeof(uint64(0)))), q, twoQ)
-		c[4], c[5] = invButterflyPow2(c[4], c[5], *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/2+2)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/2+2)*unsafe.Sizeof(uint64(0)))), q, twoQ)
-		c[6], c[7] = invButterflyPow2(c[6], c[7], *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/2+3)*unsafe.Sizeof(uint64(0)))), *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/2+3)*unsafe.Sizeof(uint64(0)))), q, twoQ)
+		c[0], c[1] = invButterflyPow2(c[0], c[1], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+0)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+0)*L)), q, twoQ)
+		c[2], c[3] = invButterflyPow2(c[2], c[3], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+1)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+1)*L)), q, twoQ)
+		c[4], c[5] = invButterflyPow2(c[4], c[5], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+2)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+2)*L)), q, twoQ)
+		c[6], c[7] = invButterflyPow2(c[6], c[7], *(*uint64)(unsafe.Add(r, uintptr(i+N/2+3)*L)), *(*uint64)(unsafe.Add(rS, uintptr(i+N/2+3)*L)), q, twoQ)
 	}
 
 	// t = 2, m = N / 4
 	for i := 0; i < N/4; i += 2 {
 		j := 4 * i
 
-		c := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
 
-		w = *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/4)*unsafe.Sizeof(uint64(0))))
-		wS = *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/4)*unsafe.Sizeof(uint64(0))))
+		w = *(*uint64)(unsafe.Add(r, uintptr(i+N/4)*L))
+		wS = *(*uint64)(unsafe.Add(rS, uintptr(i+N/4)*L))
 		c[0], c[2] = invButterflyPow2(c[0], c[2], w, wS, q, twoQ)
 		c[1], c[3] = invButterflyPow2(c[1], c[3], w, wS, q, twoQ)
 
-		w = *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/4+1)*unsafe.Sizeof(uint64(0))))
-		wS = *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/4+1)*unsafe.Sizeof(uint64(0))))
+		w = *(*uint64)(unsafe.Add(r, uintptr(i+N/4+1)*L))
+		wS = *(*uint64)(unsafe.Add(rS, uintptr(i+N/4+1)*L))
 		c[4], c[6] = invButterflyPow2(c[4], c[6], w, wS, q, twoQ)
 		c[5], c[7] = invButterflyPow2(c[5], c[7], w, wS, q, twoQ)
 	}
 
 	// t = 4, m = N / 8
 	for i := 0; i < N/8; i++ {
-		w = *(*uint64)(unsafe.Add(twInvPtr, uintptr(i+N/8)*unsafe.Sizeof(uint64(0))))
-		wS = *(*uint64)(unsafe.Add(twInvSPtr, uintptr(i+N/8)*unsafe.Sizeof(uint64(0))))
+		w = *(*uint64)(unsafe.Add(r, uintptr(i+N/8)*L))
+		wS = *(*uint64)(unsafe.Add(rS, uintptr(i+N/8)*L))
 
 		j := 8 * i
 
-		c := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
 
 		c[0], c[4] = invButterflyPow2(c[0], c[4], w, wS, q, twoQ)
 		c[1], c[5] = invButterflyPow2(c[1], c[5], w, wS, q, twoQ)
@@ -184,12 +191,12 @@ func invNTTInPlacePow2Unroll(coeffs, twInv, twInvS []uint64, q uint64) {
 			j1 := i * t << 1
 			j2 := j1 + t
 
-			w = *(*uint64)(unsafe.Add(twInvPtr, uintptr(m+i)*unsafe.Sizeof(uint64(0))))
-			wS = *(*uint64)(unsafe.Add(twInvSPtr, uintptr(m+i)*unsafe.Sizeof(uint64(0))))
+			w = *(*uint64)(unsafe.Add(r, uintptr(m+i)*L))
+			wS = *(*uint64)(unsafe.Add(rS, uintptr(m+i)*L))
 
 			for j := j1; j < j2; j += 8 {
-				c0 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
-				c1 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j+t)*unsafe.Sizeof(uint64(0))))
+				c0 := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
+				c1 := (*[8]uint64)(unsafe.Add(v, uintptr(j+t)*L))
 
 				c0[0], c1[0] = invButterflyPow2(c0[0], c1[0], w, wS, q, twoQ)
 				c0[1], c1[1] = invButterflyPow2(c0[1], c1[1], w, wS, q, twoQ)
@@ -205,11 +212,11 @@ func invNTTInPlacePow2Unroll(coeffs, twInv, twInvS []uint64, q uint64) {
 		t <<= 1
 	}
 
-	w = *(*uint64)(unsafe.Add(twInvPtr, 1*unsafe.Sizeof(uint64(0))))
-	wS = *(*uint64)(unsafe.Add(twInvSPtr, 1*unsafe.Sizeof(uint64(0))))
+	w = *(*uint64)(unsafe.Add(r, 1*L))
+	wS = *(*uint64)(unsafe.Add(rS, 1*L))
 	for j := 0; j < N/2; j += 8 {
-		c0 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j)*unsafe.Sizeof(uint64(0))))
-		c1 := (*[8]uint64)(unsafe.Add(coeffsPtr, uintptr(j+t)*unsafe.Sizeof(uint64(0))))
+		c0 := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
+		c1 := (*[8]uint64)(unsafe.Add(v, uintptr(j+t)*L))
 
 		c0[0], c1[0] = invButterflyPow2(c0[0], c1[0], w, wS, q, twoQ)
 		c0[1], c1[1] = invButterflyPow2(c0[1], c1[1], w, wS, q, twoQ)
