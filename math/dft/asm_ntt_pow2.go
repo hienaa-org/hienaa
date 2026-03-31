23d1a18c9d63f83df94bfcf3fb15289dac26b358
Intak Hwang <sp301415@gmail.com>
2026-03-31 00:27:34 +0000

    fix: Cleanup NTT

--- math/dft/asm_ntt_pow2.go
+++ math/dft/asm_ntt_pow2.go
@@ -124,72 +124,72 @@ func invNTTInPlacePow2Unroll(coeffs, twInv, twInvS []uint64, q uint64) {
 		c[6], c[7] = invButterflyPow2(c[6], c[7], w[3], wS[3], q, twoQ)
 	}
 
-	// // t = 2, m = N / 4
-	// for i := 0; i < N/4; i += 2 {
-	// 	c := (*[8]uint64)(unsafe.Add(v, uintptr(4*i)*L))
-	// 	w := (*[2]uint64)(unsafe.Add(r, uintptr(i+N/4)*L))
-	// 	wS := (*[2]uint64)(unsafe.Add(rS, uintptr(i+N/4)*L))
+	// t = 2, m = N / 4
+	for i := 0; i < N/4; i += 2 {
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(4*i)*L))
+		w := (*[2]uint64)(unsafe.Add(r, uintptr(i+N/4)*L))
+		wS := (*[2]uint64)(unsafe.Add(rS, uintptr(i+N/4)*L))
 
-	// 	c[0], c[2] = invButterflyPow2(c[0], c[2], w[0], wS[0], q, twoQ)
-	// 	c[1], c[3] = invButterflyPow2(c[1], c[3], w[0], wS[0], q, twoQ)
+		c[0], c[2] = invButterflyPow2(c[0], c[2], w[0], wS[0], q, twoQ)
+		c[1], c[3] = invButterflyPow2(c[1], c[3], w[0], wS[0], q, twoQ)
 
-	// 	c[4], c[6] = invButterflyPow2(c[4], c[6], w[1], wS[1], q, twoQ)
-	// 	c[5], c[7] = invButterflyPow2(c[5], c[7], w[1], wS[1], q, twoQ)
-	// }
+		c[4], c[6] = invButterflyPow2(c[4], c[6], w[1], wS[1], q, twoQ)
+		c[5], c[7] = invButterflyPow2(c[5], c[7], w[1], wS[1], q, twoQ)
+	}
 
-	// // t = 4, m = N / 8
-	// for i := 0; i < N/8; i++ {
-	// 	c := (*[8]uint64)(unsafe.Add(v, uintptr(8*i)*L))
-	// 	w := *(*uint64)(unsafe.Add(r, uintptr(i+N/8)*L))
-	// 	wS := *(*uint64)(unsafe.Add(rS, uintptr(i+N/8)*L))
+	// t = 4, m = N / 8
+	for i := 0; i < N/8; i++ {
+		c := (*[8]uint64)(unsafe.Add(v, uintptr(8*i)*L))
+		w := *(*uint64)(unsafe.Add(r, uintptr(i+N/8)*L))
+		wS := *(*uint64)(unsafe.Add(rS, uintptr(i+N/8)*L))
 
-	// 	c[0], c[4] = invButterflyPow2(c[0], c[4], w, wS, q, twoQ)
-	// 	c[1], c[5] = invButterflyPow2(c[1], c[5], w, wS, q, twoQ)
-	// 	c[2], c[6] = invButterflyPow2(c[2], c[6], w, wS, q, twoQ)
-	// 	c[3], c[7] = invButterflyPow2(c[3], c[7], w, wS, q, twoQ)
-	// }
+		c[0], c[4] = invButterflyPow2(c[0], c[4], w, wS, q, twoQ)
+		c[1], c[5] = invButterflyPow2(c[1], c[5], w, wS, q, twoQ)
+		c[2], c[6] = invButterflyPow2(c[2], c[6], w, wS, q, twoQ)
+		c[3], c[7] = invButterflyPow2(c[3], c[7], w, wS, q, twoQ)
+	}
 
-	// t := 8
-	// for m := N / 16; m >= 2; m >>= 1 {
-	// 	for i := 0; i < m; i++ {
-	// 		j1 := i * t << 1
-	// 		j2 := j1 + t
+	t := 8
+	for m := N / 16; m >= 2; m >>= 1 {
+		for i := 0; i < m; i++ {
+			j1 := i * t << 1
+			j2 := j1 + t
 
-	// 		w := *(*uint64)(unsafe.Add(r, uintptr(m+i)*L))
-	// 		wS := *(*uint64)(unsafe.Add(rS, uintptr(m+i)*L))
+			w := *(*uint64)(unsafe.Add(r, uintptr(m+i)*L))
+			wS := *(*uint64)(unsafe.Add(rS, uintptr(m+i)*L))
 
-	// 		for j := j1; j < j2; j += 8 {
-	// 			c0 := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
-	// 			c1 := (*[8]uint64)(unsafe.Add(v, uintptr(j+t)*L))
+			for j := j1; j < j2; j += 8 {
+				c0 := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
+				c1 := (*[8]uint64)(unsafe.Add(v, uintptr(j+t)*L))
 
-	// 			c0[0], c1[0] = invButterflyPow2(c0[0], c1[0], w, wS, q, twoQ)
-	// 			c0[1], c1[1] = invButterflyPow2(c0[1], c1[1], w, wS, q, twoQ)
-	// 			c0[2], c1[2] = invButterflyPow2(c0[2], c1[2], w, wS, q, twoQ)
-	// 			c0[3], c1[3] = invButterflyPow2(c0[3], c1[3], w, wS, q, twoQ)
+				c0[0], c1[0] = invButterflyPow2(c0[0], c1[0], w, wS, q, twoQ)
+				c0[1], c1[1] = invButterflyPow2(c0[1], c1[1], w, wS, q, twoQ)
+				c0[2], c1[2] = invButterflyPow2(c0[2], c1[2], w, wS, q, twoQ)
+				c0[3], c1[3] = invButterflyPow2(c0[3], c1[3], w, wS, q, twoQ)
 
-	// 			c0[4], c1[4] = invButterflyPow2(c0[4], c1[4], w, wS, q, twoQ)
-	// 			c0[5], c1[5] = invButterflyPow2(c0[5], c1[5], w, wS, q, twoQ)
-	// 			c0[6], c1[6] = invButterflyPow2(c0[6], c1[6], w, wS, q, twoQ)
-	// 			c0[7], c1[7] = invButterflyPow2(c0[7], c1[7], w, wS, q, twoQ)
-	// 		}
-	// 	}
-	// 	t <<= 1
-	// }
+				c0[4], c1[4] = invButterflyPow2(c0[4], c1[4], w, wS, q, twoQ)
+				c0[5], c1[5] = invButterflyPow2(c0[5], c1[5], w, wS, q, twoQ)
+				c0[6], c1[6] = invButterflyPow2(c0[6], c1[6], w, wS, q, twoQ)
+				c0[7], c1[7] = invButterflyPow2(c0[7], c1[7], w, wS, q, twoQ)
+			}
+		}
+		t <<= 1
+	}
 
-	// w := *(*uint64)(unsafe.Add(r, 1*L))
-	// wS := *(*uint64)(unsafe.Add(rS, 1*L))
-	// for j := 0; j < N/2; j += 8 {
-	// 	c0 := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
-	// 	c1 := (*[8]uint64)(unsafe.Add(v, uintptr(j+t)*L))
+	w := *(*uint64)(unsafe.Add(r, 1*L))
+	wS := *(*uint64)(unsafe.Add(rS, 1*L))
+	for j := 0; j < N/2; j += 8 {
+		c0 := (*[8]uint64)(unsafe.Add(v, uintptr(j)*L))
+		c1 := (*[8]uint64)(unsafe.Add(v, uintptr(j+t)*L))
 
-	// 	c0[0], c1[0] = invButterflyPow2(c0[0], c1[0], w, wS, q, twoQ)
-	// 	c0[1], c1[1] = invButterflyPow2(c0[1], c1[1], w, wS, q, twoQ)
-	// 	c0[2], c1[2] = invButterflyPow2(c0[2], c1[2], w, wS, q, twoQ)
-	// 	c0[3], c1[3] = invButterflyPow2(c0[3], c1[3], w, wS, q, twoQ)
+		c0[0], c1[0] = invButterflyPow2(c0[0], c1[0], w, wS, q, twoQ)
+		c0[1], c1[1] = invButterflyPow2(c0[1], c1[1], w, wS, q, twoQ)
+		c0[2], c1[2] = invButterflyPow2(c0[2], c1[2], w, wS, q, twoQ)
+		c0[3], c1[3] = invButterflyPow2(c0[3], c1[3], w, wS, q, twoQ)
 
-	// 	c0[4], c1[4] = invButterflyPow2(c0[4], c1[4], w, wS, q, twoQ)
-	// 	c0[5], c1[5] = invButterflyPow2(c0[5], c1[5], w, wS, q, twoQ)
-	// 	c0[6], c1[6] = invButterflyPow2(c0[6], c1[6], w, wS, q, twoQ)
-	// 	c0[7], c1[7] = invButterflyPow2(c0[7], c1[7], w, wS, q, twoQ)
-	// }
+		c0[4], c1[4] = invButterflyPow2(c0[4], c1[4], w, wS, q, twoQ)
+		c0[5], c1[5] = invButterflyPow2(c0[5], c1[5], w, wS, q, twoQ)
+		c0[6], c1[6] = invButterflyPow2(c0[6], c1[6], w, wS, q, twoQ)
+		c0[7], c1[7] = invButterflyPow2(c0[7], c1[7], w, wS, q, twoQ)
+	}
 }
